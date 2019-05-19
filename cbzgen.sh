4dd06919b581bab592d1de62611c29592ec198d0
Iris W <46726098+wildthyme@users.noreply.github.com>
2019-05-19 12:18:17 -0400

    made cbzgen stop tryna human centipede itself

--- cbzgen.sh
+++ cbzgen.sh
@@ -3,7 +3,8 @@ DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 outputFolder=`node -pe 'JSON.parse(process.argv[1]).outputFolder' "$(cat $DIR/config.json)"`
 node $DIR/index.js
 for i in `ls -1 "$outputFolder"`; do
-  if [[ $i != json ]]; then
+  if [[ $i != json ]] && [[ $i != *.cbz ]]; then
+    echo true
     for j in `ls -1 "$outputFolder/$i"`; do
       zip -ju "$outputFolder/$i$j.cbz" "$outputFolder/$i/$j/"*/*
   done;
